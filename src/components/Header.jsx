d4d3b692f148552c5154b8d1513e43958cb8b563
shifachaus <chausshifa804@gmail.com>
2024-05-22 13:16:43 +0530

    Add rendering of GIF categories

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { HiEllipsisVertical, HiMiniBars3BottomRight } from "react-icons/hi2";
+import { useGifSState } from "../context/gif-context";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
   const [showCategories, setShowCategories] = useState(false);
+
+  const { gf, gifs, filter, favorites } = useGifSState();
+
+  const fetchGifCategories = async () => {
+    const { data } = await gf.categories();
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    fetchGifCategories();
+  }, []);
 
   return (
     <nav>
@@ -15,41 +27,62 @@ const Header = () => {
             GIPHY
           </h1>
         </Link>
+        <div className="font-bold text-md flex gap-2 items-center">
+          {/* Categories */}
 
-        <Link
-          to={""}
-          className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
-        >
-          Reactions
-        </Link>
+          {categories.slice(0, 5).map((category) => {
+            return (
+              <Link
+                key={category.name}
+                to={`/${category.name_encoded}`}
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+              >
+                {category.name}
+              </Link>
+            );
+          })}
 
-        <button onClick={() => setShowCategories(!showCategories)}>
-          <HiEllipsisVertical
-            size={35}
-            className={`py-0.5 transition ease-in-out hover:gradient ${
-              showCategories ? "gradient" : ""
-            } border-b-4 cursor-pointer hidden lg:block`}
-          />
-        </button>
+          <button onClick={() => setShowCategories(!showCategories)}>
+            <HiEllipsisVertical
+              size={35}
+              className={`py-0.5 transition ease-in-out hover:gradient ${
+                showCategories ? "gradient" : ""
+              } border-b-4 cursor-pointer hidden lg:block`}
+            />
+          </button>
 
-        <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
-          <Link to="/favorites">Favorite GIFs</Link>
+          {favorites.length > 0 && (
+            <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+              <Link to="/favorites">Favorite GIFs</Link>
+            </div>
+          )}
+
+          {/* -- Mobile UI -- */}
+          <button onClick={() => setShowCategories(!showCategories)}>
+            <HiMiniBars3BottomRight
+              className="text-sky-400 block lg:hidden"
+              size={30}
+            />
+          </button>
         </div>
-
-        {/* -- Mobile UI -- */}
-        <button onClick={() => setShowCategories(!showCategories)}>
-          <HiMiniBars3BottomRight
-            className="text-sky-400 block lg:hidden"
-            size={30}
-          />
-        </button>
 
         {showCategories && (
           <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
             <span className="text-3xl font-extrabold">Categories</span>
             <hr className="bg-gray-100 opacity-50 my-5" />
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-              <Link to="/"> Reactions</Link>
+              {categories?.map((category) => {
+                return (
+                  <Link
+                    onClick={() => setShowCategories(false)}
+                    className="transition ease-in-out font-bold"
+                    key={category.name}
+                    to={`/${category.name_encoded}`}
+                  >
+                    {category.name}
+                  </Link>
+                );
+              })}
             </div>
           </div>
         )}
--- src/context/gif-context.jsx
+++ src/context/gif-context.jsx
@@ -1,9 +1,24 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
 
 const GifContext = createContext();
 
 const GifProvider = ({ children }) => {
-  return <GifContext.Provider value={"l"}>{children}</GifContext.Provider>;
+  const [gifs, setGifs] = useState([]);
+  const [filter, setFilter] = useState("gifs");
+  const [favorites, setFavorites] = useState([]);
+
+  const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_KEY);
+
+  return (
+    <GifContext.Provider value={{ gf, gifs, filter, favorites }}>
+      {children}
+    </GifContext.Provider>
+  );
+};
+
+export const useGifSState = () => {
+  return useContext(GifContext);
 };
 
 export default GifProvider;
