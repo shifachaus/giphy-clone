3c0fc91f03dc0a7cf183e0c128faca852145c4f4
shifachaus <chausshifa804@gmail.com>
2024-05-22 12:18:11 +0530

    add react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Category from "./pages/Category";
+import Search from "./pages/Search";
+import SignleGif from "./pages/SignleGif";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:type/:slug",
+        element: <SignleGif />,
+      },
+      {
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+        element: <Search />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/SignleGif.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignleGif = () => {
+  return <div>SignleGif</div>;
+};
+
+export default SignleGif;
